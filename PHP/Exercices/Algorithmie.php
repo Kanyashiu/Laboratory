fdae470b1c25b4797b0c3b1dc7f412699e61ab96
Kanyashiu <maazaoui.karim1@gmail.com>
2020-07-28 16:38:37 +0200

    Creation Exercices Folder

---
+++ PHP/Exercices/Algorithmie.php
@@ -0,0 +1,6 @@
+<?php
+
+//==================================
+// DISCLAIMER
+// Source des exercices : https://www.fustel-yaounde.net/sites/default/files/Doucments-2017-03/exercices%20corrig%C3%A9s%20algorithme.pdf
+//==================================
---
+++ PHP/Exercices/Debutant.php
@@ -0,0 +1,102 @@
+<?php
+
+//==================================
+// DISCLAIMER
+// Source des exercices : https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+//==================================
+
+// Exercice 1, Faite en sorte que la fonction HelloWorld retourne exactement la valeur Hello World!
+
+// Exercice 2, Créer une fonction from scratch qui s'appelle quiEstLeMeilleurProf(). Elle doit retourner Le prof de programmation Web
+
+// Exercice 3, Créer une fonction from scratch qui s'appelle jeRetourneMonArgument(). Exemple : Arg = "abc" ==> Return abc Arg = 123 ==> Return 123
+
+// Exercice 4, Créer une fonction from scratch qui s'appelle concatenation().
+// Elle prendra deux arguments de type string. Elle devra retourner la concatenation des deux.
+// Exemple : argument 1 = Antoine Argument 2 = Griezmann; Resultat : AntoineGriezmann
+
+// Exercice 5, Créer une fonction from scratch qui s'appelle concatenationAvecEspace().
+// Elle prendra deux arguments de type string. Elle devra retourner la concatenation des deux.
+// Exemple : argument 1 = Ngolo Argument 2 = Kante; Resultat : Ngolo Kante
+
+// Exercice 6, Créer une fonction from scratch qui s'appelle somme().
+// Elle prendra deux arguments de type int. Elle devra retourner la somme des deux.
+// Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 10
+
+// Exercice 7, Créer une fonction from scratch qui s'appelle soustraction().
+// Elle prendra deux arguments de type int. Elle devra retourner la soustraction des deux. 
+// Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 0
+
+// Exercice 8, Créer une fonction from scratch qui s'appelle multiplication().
+// Elle prendra deux arguments de type int. Elle devra retourner la multiplication des deux. 
+// Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 25
+
+// Exercice 9, Créer une fonction from scratch qui s'appelle estIlMajeure().
+// Elle prendra un argument de type int. Elle devra retourner un boolean.
+// Si age >= 18 elle doit retourner true si age < 18 elle doit retourner false Exemple : age = 5 ==> false age = 34 ==> true
+
+// Exercice 10, Créer une fonction from scratch qui s'appelle plusGrand().
+// Elle prendra deux arguments de type int. Elle devra retourner le plus grand des deux.
+
+// Exercice 11, Créer une fonction from scratch qui s'appelle plusPetit().
+// Elle prendra deux arguments de type int. Elle devra retourner le plus petit des deux.
+
+// Exercice 12, Créer une fonction from scratch qui s'appelle plusPetit(). Elle prendra trois arguments de type int. 
+// Elle devra retourner le plus petit des trois.
+
+// Exercice 13, Créer une fonction from scratch qui s'appelle premierElementTableau(). 
+// Elle prendra un argument de type array. Elle devra retourner le premier élement du tableau. Si l'array est vide, il faudra retourner null
+
+// Exercice 14, Créer une fonction from scratch qui s'appelle dernierElementTableau(). 
+// Elle prendra un argument de type array. Elle devra retourner le dernier élement du tableau. Si l'array est vide, il faudra retourner null;
+
+// Exercice 15, Créer une fonction from scratch qui s'appelle plusGrand(). 
+// Elle prendra un argument de type array. Elle devra retourner le plus grand des élements présent dans l'array. Si l'array est vide, il faudra retourner null;
+
+// Exercice 16, Créer une fonction from scratch qui s'appelle plusPetit(). 
+// Elle prendra un argument de type array. Elle devra retourner le plus petit des élements présent dans l'array. Si l'array est vide, il faudra retourner null;
+
+// Exercice 17, Créer une fonction from scratch qui s'appelle verificationPassword(). Elle prendra un argument de type string. 
+// Elle devra retourner un boolean qui vaut true si le password fait au moins 8 caractères et false si moins.
+
+// Exercice 18, Créer une fonction from scratch qui s'appelle verificationPassword(). 
+// Elle prendra un argument de type string. 
+// Elle devra retourner un boolean qui vaut true si le password respecte les règles suivantes :
+// Faire au moins 8 caractères
+// Avoir au moins 1 chiffre
+// Avoir au moins une majuscule et une minuscule
+
+
+// Exercice 19, Créer une fonction from scratch qui s'appelle capital(). 
+// Elle prendra un argument de type string. Elle devra retourner le nom de la capitale des pays suivants :
+// France ==> Paris
+// Allemagne ==> Berlin
+// Italie ==> Rome
+// Maroc ==> Rabat
+// Espagne ==> Madrid
+// Portugal ==> Lisbonne
+// Angleterre ==> Londres
+// Tout autre pays ==> Inconnu
+// Il faudra utiliser la structure SWITCH pour faire cette exercice.
+
+// Exercice 20, Créer une fonction from scratch qui s'appelle listHTML(). Elle prendra deux arguments :
+// Un string représentant le nom de la liste
+// Un array représentant les élements de cette liste
+// Elle devra retourner une liste HTML. Chaque element de cette liste viendra du tableau passé en paramètre.
+// Exemple : Paramètre : Titre : Capitale Liste : ["Paris", "Berlin", "Moscou"] Résultat : <h3>Capitale</h3><ul><li>Paris</li><li>Berlin</li><li>Moscou</li></ul>
+// Comme vous pouvez le voir il n'y a pas d'espace ni de retour à la ligne entre les élements de la liste. Pas d'espace non plus entre le titre et la liste.
+// Si le titre est null et vide il faut que la fonction retourne null. Si l'array est vide, il faut que la fonction retourne null.
+
+// Exercice 21, Créer une fonction from scratch qui s'appelle remplacerLesLettres(). 
+// Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 
+// Exemple :
+// input : "Bonjour les amis" ==> Output : B0nj0ur l3s am1s
+// input : "Les cours de programmation Web sont trops cools" ==> Output : L3s c0urs d3 pr0grammat10n W3b s0nt tr0ps c00ls
+
+
+// Exercice 22, Créer une fonction from scratch qui s'appelle quelleAnnee(). Elle devra retourner un integer representant l'année actuelle.
+
+// Exercice 23, Créer une fonction from scratch qui s'appelle quelleDate(). 
+// Elle devra retourner une string representant la date actuelle sous le format suivant DD/MM/YYYY
+// Où DD représente le jour actuelle, MM le mois actuel et YYYY l'année actuelle. Les valeurs doivent être numérique et non au format String.
+
