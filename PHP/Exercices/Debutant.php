e15643e1700a2669c56084ae1677ce409985972e
Kanyashiu <maazaoui.karim1@gmail.com>
2020-07-28 18:14:51 +0200

    Update Debutant.php

--- PHP/Exercices/Debutant.php
+++ PHP/Exercices/Debutant.php
@@ -2,7 +2,9 @@
 
 //==================================
 // DISCLAIMER
-// Source des exercices : https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+// Source des exercices 1 à 23 : https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+// Source des exercices 24 à 33 : https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+// Source des exercices 34 à 33 : https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
 // Ce fichier est un énoncé d'exercices pour php, les corrections sont dans le dossier correspondant
 //==================================
 
@@ -60,39 +62,43 @@
 // Exercice 17, Créer une fonction from scratch qui s'appelle verificationPassword(). Elle prendra un argument de type string. 
 // Elle devra retourner un boolean qui vaut true si le password fait au moins 8 caractères et false si moins.
 
-// Exercice 18, Créer une fonction from scratch qui s'appelle verificationPassword(). 
-// Elle prendra un argument de type string. 
-// Elle devra retourner un boolean qui vaut true si le password respecte les règles suivantes :
-// Faire au moins 8 caractères
-// Avoir au moins 1 chiffre
-// Avoir au moins une majuscule et une minuscule
+/* Exercice 18, Créer une fonction from scratch qui s'appelle verificationPassword(). 
+Elle prendra un argument de type string. 
+Elle devra retourner un boolean qui vaut true si le password respecte les règles suivantes :
+Faire au moins 8 caractères
+Avoir au moins 1 chiffre
+Avoir au moins une majuscule et une minuscule
+*/
 
 
-// Exercice 19, Créer une fonction from scratch qui s'appelle capital(). 
-// Elle prendra un argument de type string. Elle devra retourner le nom de la capitale des pays suivants :
-// France ==> Paris
-// Allemagne ==> Berlin
-// Italie ==> Rome
-// Maroc ==> Rabat
-// Espagne ==> Madrid
-// Portugal ==> Lisbonne
-// Angleterre ==> Londres
-// Tout autre pays ==> Inconnu
-// Il faudra utiliser la structure SWITCH pour faire cette exercice.
+/* Exercice 19, Créer une fonction from scratch qui s'appelle capital(). 
+Elle prendra un argument de type string. Elle devra retourner le nom de la capitale des pays suivants :
+France ==> Paris
+Allemagne ==> Berlin
+Italie ==> Rome
+Maroc ==> Rabat
+Espagne ==> Madrid
+Portugal ==> Lisbonne
+Angleterre ==> Londres
+Tout autre pays ==> Inconnu
+Il faudra utiliser la structure SWITCH pour faire cette exercice.
+*/
 
-// Exercice 20, Créer une fonction from scratch qui s'appelle listHTML(). Elle prendra deux arguments :
-// Un string représentant le nom de la liste
-// Un array représentant les élements de cette liste
-// Elle devra retourner une liste HTML. Chaque element de cette liste viendra du tableau passé en paramètre.
-// Exemple : Paramètre : Titre : Capitale Liste : ["Paris", "Berlin", "Moscou"] Résultat : <h3>Capitale</h3><ul><li>Paris</li><li>Berlin</li><li>Moscou</li></ul>
-// Comme vous pouvez le voir il n'y a pas d'espace ni de retour à la ligne entre les élements de la liste. Pas d'espace non plus entre le titre et la liste.
-// Si le titre est null et vide il faut que la fonction retourne null. Si l'array est vide, il faut que la fonction retourne null.
+/* Exercice 20, Créer une fonction from scratch qui s'appelle listHTML(). Elle prendra deux arguments :
+Un string représentant le nom de la liste
+Un array représentant les élements de cette liste
+Elle devra retourner une liste HTML. Chaque element de cette liste viendra du tableau passé en paramètre.
+Exemple : Paramètre : Titre : Capitale Liste : ["Paris", "Berlin", "Moscou"] Résultat : <h3>Capitale</h3><ul><li>Paris</li><li>Berlin</li><li>Moscou</li></ul>
+Comme vous pouvez le voir il n'y a pas d'espace ni de retour à la ligne entre les élements de la liste. Pas d'espace non plus entre le titre et la liste.
+Si le titre est null et vide il faut que la fonction retourne null. Si l'array est vide, il faut que la fonction retourne null.
+*/
 
-// Exercice 21, Créer une fonction from scratch qui s'appelle remplacerLesLettres(). 
-// Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 
-// Exemple :
-// input : "Bonjour les amis" ==> Output : B0nj0ur l3s am1s
-// input : "Les cours de programmation Web sont trops cools" ==> Output : L3s c0urs d3 pr0grammat10n W3b s0nt tr0ps c00ls
+/* Exercice 21, Créer une fonction from scratch qui s'appelle remplacerLesLettres(). 
+Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 
+Exemple :
+input : "Bonjour les amis" ==> Output : B0nj0ur l3s am1s
+input : "Les cours de programmation Web sont trops cools" ==> Output : L3s c0urs d3 pr0grammat10n W3b s0nt tr0ps c00ls
+*/
 
 
 // Exercice 22, Créer une fonction from scratch qui s'appelle quelleAnnee(). Elle devra retourner un integer representant l'année actuelle.
@@ -101,3 +107,111 @@
 // Elle devra retourner une string representant la date actuelle sous le format suivant DD/MM/YYYY
 // Où DD représente le jour actuelle, MM le mois actuel et YYYY l'année actuelle. Les valeurs doivent être numérique et non au format String.
 
+
+// Exercice 24, Acronyme : Créez une fonction qui prend en argument un string et qui retourne les initiales de chaque mot en majuscule.
+
+// Exercice 25, Créez une fonction qui prend en argument un string et qui inverse cette chaîne de caractères;
+
+// Exercice 26, Bataille Navale
+/*
+Image : https://user.oc-static.com/files/175001_176000/175985.png
+Ci-dessus un tableau de 10 cases sur 10, vous devez créez une fonction qui prend deux arguments, le premier de type char le second de type int, ce seront les coordonnées horizontales et verticales du tableau.
+Votre fonction doit retourner trois valeurs différentes:
+[1] Si les coordonnées correspondent à une case grise, votre fonction doit afficher touché.
+[2] Si les coordonnées correspondent à une case blanche, votre fonction doit afficher loupé.
+[3] Si les coordonnées ne correspondent à aucune case, votre fonction doit afficher hors-jeu.
+ Contrainte: Vous ne devez pas dépasser 30 lignes, avec un code correctement indenté.
+*/
+
+// Exercice 27, Conjecture de Syracuse : Créez une fonction qui prend comme argument n'importe quel nombre entier supérieur à zéro, 
+// si le nombre est pair divisez-le par 2, si le nombre est impair multipliez-le par 3 et ajoutez 1. 
+// Au final votre fonction doit retourner une série de nombre, le dernier de ces nombres doit être le chiffre 1.
+
+// Exercice 28, Triangle : Créez une fonction qui prend un argument de type int, cela déterminera la hauteur (en ligne) d'un triangle isocèle rempli d'étoiles
+/* Exemple :
+       **       
+      ****      
+     ******     
+    ********    
+   **********   
+  ************  
+ ************** 
+****************
+*/
+
+// Exercice 29, Suite de Fibonacci
+// « Possédant initialement un couple de lapins, combien de couples obtient-on en douze mois si chaque couple engendre tous les mois un nouveau couple à compter du second mois de son existence ? »
+// Creez une fonction qui prend un argument de type int, cela correspondra à la durée en mois de la reproduction des lapins, votre fonction doit retourner le nombre de couples.
+
+/* Exercice 30, Conjugaison
+Créez une fonction qui prend une string en argument, l'argument sera un verbe régulier du premier groupe (finissant par "er"). Votre fonction doit conjuguer ce verbe au présent de l'indicatif.
+Contrainte:
+La chaîne passée en argument ne doit pas dépasser 15 caractères ni contenir d'espaces.
+Vérifiez que la chaîne passée en argument se termine bien par "er".
+*/
+
+/* Exercice 31, Table de multiplication binaire
+Créez une fonction qui prend en argument un entier positif non nul, cela correspondra au nombre de lignes et de colonnes d'une table de multiplication. Tous les nombres affichés doivent être des nombres en base binaire. 
+Vous devez séparer les nombres sur une ligne par des caractères de tabulation.
+Remarque:
+Vous pouvez utiliser la fonction decbin() si vous avez du mal à résoudre cet exercice, cependant il est possible et plus interessant de ne pas l'utiliser.
+Exemple :
+En entrée : 6
+En sortie :
+1       10       11       100       101       110
+10      100      110      1000      1010      1100
+11      110      1001     1100      1111      10010
+100     1000     1100     10000     10100     11000
+101     1010     1111     10100     11001     11110
+110     1100     10010    11000     11110     100100
+
+Commencez par la gauche et ajoutez un 0 ou un 1 à chaque case du tableau pour obtenir le nombre que vous souhaitez.
+*/
+
+// Exercice 32, Le loup, la chèvre et le chou
+/* Un berger accompagné d'un loup, d'une chèvre et d'un chou, traverse tous les jours une rivière. Le berger ne possède qu'une seule barque. 
+La barque ne peut prendre qu'un passager à la fois (plus le berger qui conduit). Sachant qu'en absence du berger, le loup mange la chèvre, et la chèvre mange le chou.
+Créez une fonction qui prend un argument de type int, cela correspondra au nombre de traversées de la rivière. 
+Votre fonction doit retourner la position du berger, du loup, de la chèvre et du chou suivant la rive où ils se trouvent, 
+en prenant compte que ni la chèvre ni le chou ne doivent être mangés.
+Exemple :
+En entrée:
+1
+
+
+En sortie:
+Le berger est sur la rive droite.
+Le loup est sur la rive gauche.
+La chèvre est sur la rive droite.
+Le chou est sur la rive gauche.
+*/
+
+// Exercice 33, Le problème de Josephus Flavius
+/* 
+Selon une légende datant du 1er siècle après J-C, Josephus Flavius et ses compatriotes, 
+pris au piège dans une grotte par les romains, et préférant la mort à la capture, décidèrent de former un cercle et de tuer la troisième personne vivante rencontrée en suivant le parcours autour du cercle. 
+Josephus Flavius ne voulant pas mourir, trouva rapidement la bonne place et fût la seule personne à sortir vivante de la grotte.
+Créez une fonction prenant deux arguments de type int, le premier correspondra au nombre de personnes formant le cercle, 
+le second sera le nombre de personnes comptées avant l'exécution de la suivante. Votre fonction devra retourner la position initiale de la dernière personne vivante.
+*/
+
+/* Exercice 34,
+Vous devez mélanger les lettres d'un mot, en gardant la première et dernière lettre.
+Les apostrophes, virgules, points etc. ne comptent pas (pour que ça reste facile). Le texte doit être entièrement en minuscule, peut importe l'entrée.
+Exemple :
+Entrée = "Même si les lettres dans un mot sont dans le désordre on peut lire ce mot"
+Sortie = mmêe si les lterets dnas un mot snot dans le désrrdoe on puet lire ce mot"
+*/
+
+/* Exercice 35,
+Votre fonction prendra 2 arguments :
+Un array de mot entièrement en minuscule : exemple : array("bonjour", "test", salut", "bonsoir", "syphopowa")
+Une série de lettres espacés d'un espace, en minuscule ou majuscule, une lettre peut être répêté plusieurs fois : Exemple : A e K J D o L A p O
+Vous devrez écrire (pas retourner) tous les mots, en allant à la ligne pour chaque mot, qui sont dans l'array entré, qui peuvent se composer à partir des lettres du 2eme argument, en utilisant chaque lettre une seule fois (par mot bien sûr) !
+Exemple :
+Entrée = <?php fonction(array("bonjour", "test", "salut", "bonsoir", "syphopowa"), "b o O K s L t U A N r I J"); ?>
+Sortie = 
+bonjour
+salut
+bonsoir
+*/
