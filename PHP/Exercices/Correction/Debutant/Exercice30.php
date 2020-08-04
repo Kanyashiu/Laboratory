2018dcf65d101cde658f054f26d2d7d4f295cdc6
Kanyashiu <maazaoui.karim1@gmail.com>
2020-08-04 22:31:01 +0200

    Exercice 30 Debutant done

--- PHP/Exercices/Correction/Debutant/Exercice30.php
+++ PHP/Exercices/Correction/Debutant/Exercice30.php
@@ -10,6 +10,34 @@ Vérifiez que la chaîne passée en argument se termine bien par "er".
 
 function verbe($string) {
     
+    if (preg_match("/^.+(er)$/", $string) && strlen($string) <= 15) {
+        
+        $conjugaison = [
+            "je" => "e",
+            "tu" => "es",
+            "il" => "e",
+            "nous" => "ons",
+            "vous" => "ez",
+            "ils" => "ent",
+        ];
+
+        foreach ($conjugaison as $pronom => $terminaison) {
+            echo $pronom . " " . substr($string, 0, -2) . $terminaison . PHP_EOL; 
+        }
+    }
+    else {
+        return "Le verbe doit faire au maximum 15 caractère et être un verbe du 1er groupe";
+    }
 };
 
 echo verbe('fermer') . PHP_EOL;
+echo verbe('chanter') . PHP_EOL;
+
+
+// echo verbe('manger') . PHP_EOL; // Mal conjugé avec le code ci-dessus
+// echo verbe('balayer') . PHP_EOL; // Mal conjugé avec le code ci-dessus
+// echo verbe('placer') . PHP_EOL; // Mal conjugé avec le code ci-dessus
+// echo verbe('appeler') . PHP_EOL; // Mal conjugé avec le code ci-dessus
+// echo verbe('acheter') . PHP_EOL; // Mal conjugé avec le code ci-dessus
+
+
