27e0efc5719591ba95a3e452e402373008d5c68d
Kanyashiu <maazaoui.karim1@gmail.com>
2020-08-06 22:49:45 +0200

    Update Final.php

--- PHP/Laboratoire/Classe/Final.php
+++ PHP/Laboratoire/Classe/Final.php
@@ -6,26 +6,31 @@
 // Si vous voulez tester ce script placer vous dans le dossier contenant ce script avec votre console et taper "php nom-du-script.php"
 //==================================
 
-echo '<br>==============TEST Mot clé Final================<br>';
+echo "\e[1;33m\033[32m================================" . PHP_EOL;
+echo "FINAL" . PHP_EOL;
+echo "================================\033[0m" . PHP_EOL;
 
-//! EXEMPLE MOT CLE FINAL
-//https://www.php.net/manual/fr/language.oop5.final.php
+// Le mot clé final empêche les classe filles de surcharger une méthode contenu dans leur parent
+// cette déclaration se fait par le mot clé devant la méthode,
+// on peut aussi empêcher qu'une classe soit étendue ( héritage ) en mettant le mot clé devant la classe
+// Documentation sur le mot clé final :
+// https://www.php.net/manual/fr/language.oop5.final.php
 
 
+echo "========= METHODES FINALE ==========" . PHP_EOL;
 class FinalClass
 {
 
     // Quand une méthode contient le mot clé final
-    // Elle ne peut pas être écrasé par les enfant
-    // elle conserve donc sa forme
+    // elle ne peut pas être écrasé par les enfants
     final public static function salut()
     {
-        return 'salut <br>';
+        return 'Salut' . PHP_EOL;
     }
 
     public static function crusoe()
     {
-        return 'crusoe <br>';
+        return 'Crusoé' . PHP_EOL;
     }
 }
 
@@ -37,7 +42,7 @@ class FinalChild extends FinalClass
     // elle ne peut donc pas écrasé la méthode de son parent
     // public static function salut()
     // {
-    //     return 'salut Toi <br>';
+    //     return 'Salut toi' . PHP_EOL;
     // }
 
 }
@@ -45,8 +50,26 @@ class FinalChild extends FinalClass
 $finalClass = new FinalClass();
 $finalChild = new FinalChild();
 
-echo $finalClass::salut(); // salut
-echo $finalClass::crusoe(); // crusoe
+echo $finalClass::salut(); // Sortie : Salut
+echo $finalClass::crusoe(); // Sortie : Crusoé
 
-//echo $finalChild::salut(); //Fatal error: Cannot override final method FinalClass::salut()
-echo $finalClass::crusoe(); // crusoe
+// echo $finalChild::salut(); // Génère une erreur fatale ou la sortie attendu selon le cas surchargé ou non
+echo $finalClass::crusoe(); // Sortie : Crusoé
+echo "\f\f";
+
+
+echo "========= CLASSE FINALE ==========" . PHP_EOL;
+final class LastFinalClass {
+
+}
+
+// Cette déclaration de classe créera un erreur fatale
+// PHP Fatal error:  Class LastFinalChild may not inherit from final class (LastFinalClass)
+// Une classe déclaré comme final, ne peut étendue ( notion d'héritage )
+
+/*
+class LastFinalChild extends LastFinalClass {
+
+
+}
+*/
