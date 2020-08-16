93d7bb6eb3602e9f8328805c88a53c05ac93d7bd
Kanyashiu <maazaoui.karim1@gmail.com>
2020-08-16 22:42:45 +0200

    Update TypeHinting.php

--- PHP/Laboratoire/Classe/TypeHinting.php
+++ PHP/Laboratoire/Classe/TypeHinting.php
@@ -44,7 +44,12 @@ class TypeHinting
 
     public function checkBool (bool $bool)
     {
-        return $bool . PHP_EOL;
+        return $bool;
+    }
+
+    public function checkNull ( ?int $null)
+    {
+        return $null;
     }
 }
 
@@ -52,6 +57,7 @@ $typeHinting = new TypeHinting();
 
 
 echo "==============INTEGER ( NOMBRE ENTIER )================" . PHP_EOL;
+// Documentation sur les entiers : https://www.php.net/manual/fr/language.types.integer.php
 
 echo $typeHinting->checkInt(1); // Sortie : 1
 
@@ -65,12 +71,26 @@ echo $typeHinting->checkInt('2'); // Sortie : 2
 
 // echo $typeHinting->checkInt('3ZNZZZZZ'); // Sortie : 3
 // echo $typeHinting->checkInt('4ZNZZZZZ522222'); Sortie : 4
+
+// Nous pouvons pousser la vérification des entiers plus loin en utilisant d'autre système de numération
+
+// Nombre octal ( Base 8 ) : https://fr.wikipedia.org/wiki/Syst%C3%A8me_octal
+echo "Octal => " . $typeHinting->checkInt(0210); // Sortie : 136 ( Base 10 )
+
+// Hexadécimal ( Base 16 ) : https://fr.wikipedia.org/wiki/Syst%C3%A8me_hexad%C3%A9cimal
+echo "Hexadécimal => " . $typeHinting->checkInt(0x1A0); // Sortie : 416
+
+// Binaire ( Base 2 ) : https://fr.wikipedia.org/wiki/Syst%C3%A8me_hexad%C3%A9cimal
+echo "Binaire => " . $typeHinting->checkInt(0b011101101010); // Sortie : 1898
 echo "\f\f";
 
 
 echo "==============FLOAT ( NOMBRE FLOTTANT )================" . PHP_EOL;
+// Documentation sur les nombre flottant : https://www.php.net/manual/fr/language.types.float.php
 
 echo $typeHinting->checkFloat(1.5); // Sortie : 1.5
+echo $typeHinting->checkFloat(1.5e5); // Sortie : 150000
+echo $typeHinting->checkFloat(700000E-3); // Sortie : 700
 
 // PHP est capable de parser une chaine de caractère ( string ) qui est un chiffre en float
 echo $typeHinting->checkFloat('2.5'); // Sortie : 2.5
@@ -91,21 +111,39 @@ echo "\f\f";
 echo "==============BOOL ( BOOLEEN )================" . PHP_EOL;
 // Documentation sur les booléen : https://www.php.net/manual/fr/language.types.boolean.php
 
-///echo $typeHinting->checkBool(true); // 1 OK
-///echo $typeHinting->checkBool(false); // OK // le false n'affiche rien à l'écran
-// php est capable de parser en suivant la régle
-// true => 1, 'string'
-// false => 0, ''
-///echo '<br> Cas true <br>';
-///echo $typeHinting->checkBool('string'); // 1 OK
-///echo $typeHinting->checkBool(1); // 1 OK 
-///echo $typeHinting->checkBool(1.1); // 1 OK 
-//echo $typeHinting->checkBool(['tableau']); // Erreur Fatale Uncaught TypeError: Argument 1 passed to TypeHinting::checkBool() must be of the type bool, array given 
-///echo '<br> Cas true <br>';
-///echo $typeHinting->checkBool(''); // 1 OK 
-///echo $typeHinting->checkBool(0); // OK
-///echo $typeHinting->checkBool(0.0); // OK 
-//echo $typeHinting->checkBool(null); // Erreur Fatale Uncaught TypeError: Argument 1 passed to TypeHinting::checkBool() must be of the type bool, null given
+var_dump($typeHinting->checkBool(true)); PHP_EOL; // Sortie : true
+var_dump($typeHinting->checkBool(false)); PHP_EOL; // Sortie : false
+echo "\f\f";
+
+echo "================On vérifie des booleens avec des variable de type différent ( True )================" . PHP_EOL;
+var_dump($typeHinting->checkBool('string')); PHP_EOL; // Sortie : true
+var_dump($typeHinting->checkBool(1)); PHP_EOL; // Sortie : true
+var_dump($typeHinting->checkBool(-1)); PHP_EOL; // Sortie : true  
+var_dump($typeHinting->checkBool(1.1)); PHP_EOL; // Sortie : true 
+var_dump($typeHinting->checkBool(-1.1)); PHP_EOL; // Sortie : true 
+// var_dump($typeHinting->checkBool(['tableau'])); PHP_EOL; // Erreur Fatale Uncaught TypeError: Argument 1 passed to TypeHinting::checkBool() must be of the type bool, array given
+echo "\f\f";
+
+echo "================On vérifie des booleens avec des variable de type différent ( False )================" . PHP_EOL;
+var_dump($typeHinting->checkBool('')); PHP_EOL; // Sortie : false
+var_dump($typeHinting->checkBool(0)); PHP_EOL; // Sortie : false
+var_dump($typeHinting->checkBool(-0)); PHP_EOL; // Sortie : false  
+var_dump($typeHinting->checkBool(0.0)); PHP_EOL; // Sortie : false 
+var_dump($typeHinting->checkBool(-0.0)); PHP_EOL; // Sortie : false 
+// var_dump($typeHinting->checkBool([])); PHP_EOL; // Erreur Fatale Uncaught TypeError: Argument 1 passed to TypeHinting::checkBool() must be of the type bool, array given
+echo "\f\f";
+
+// On peut voir que contrairement à d'autre langage, les variable dans php ne sont pas très typé
 
 
-// Null
+echo "==============NULL================" . PHP_EOL;
+// Documentation sur null : https://www.php.net/manual/fr/language.types.null.php
+
+// Pour le null il n'y pas grand chose au sujet de TypeHinting, elle représente une variable sans valeur
+var_dump($typeHinting->checkNull(null)); PHP_EOL; // Sortie : Null
+echo "\f\f";
+
+echo "==============STRING ( chaine de caractère ) ================" . PHP_EOL;
+// Documentation sur les chaine de caractère : https://www.php.net/manual/fr/language.types.string.php
+
+// TODO
