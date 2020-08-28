f11e54bf0d997f25d66aff04d31341b0ab76fdf2
Kanyashiu <maazaoui.karim1@gmail.com>
2020-08-28 11:44:52 +0200

    Update FonctionAnonyme.php

--- PHP/Laboratoire/Fonction/FonctionAnonyme.php
+++ PHP/Laboratoire/Fonction/FonctionAnonyme.php
@@ -18,13 +18,42 @@ echo "FONCTION ANONYME" . PHP_EOL;
 echo "================================\033[0m" . PHP_EOL;
 
 
-echo "========= Fonction anonymes ==========" . PHP_EOL;
-
-
 // De la même manière que javascript en entourant la fonction anonymes dans des parenthèses on l'éxécute directement
+
+// Sans Arguments
+// Version PHP 7.x
 (function(){
-    echo 'Fonction anonyme bien exécutée' . PHP_EOL;
+    echo 'Fonction anonyme 7.x bien exécutée' . PHP_EOL;
 })();
+// Version PHP 5.x
+(call_user_func(function(){
+    echo 'Fonction anonyme 5.x bien exécutée' . PHP_EOL;
+}));
+
+// Avec Arguments
+// Version PHP 7.x
+(function($name){
+    echo 'Hello' . $name . PHP_EOL;
+})('Karim 7.x');
+
+// Version PHP 5.x
+(call_user_func(function($name){
+    echo 'Hello' . $name . PHP_EOL;
+}, 'Karim 5.x'));
 
 
-// TODO
+// Dans ce cas la notre fonction anonymes à été déclarée dans une variable
+// PHP va convertir cette variable en objet Closure
+// Documentation sur la classe Closure : https://www.php.net/manual/fr/class.closure.php
+$multiplication = function($x) {
+    return $x*2;
+};
+
+$tableau = [1, 2, 3, 4, 5, 6, 8, 15];
+
+// array_map permet d'appliquer une fonction de rappel ( 1er argument)
+// sur un tableau ( 2ème arguement )
+// Documentation : https://www.php.net/manual/fr/function.array-map.php
+$multiplication_tableau = array_map($multiplication, $tableau);
+
+print_r($multiplication_tableau); PHP_EOL;
